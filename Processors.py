69a85e3161b55e27af118326f0616906c2d5bdbc
Milena <milenamalush1@gmail.com>
2016-02-02 00:01:20 +0200

    'initial_commit'

---
+++ Processors.py
@@ -0,0 +1,21 @@
+class Processors:
+    def __init__(self, amount):
+        self.amount = amount
+        self.dict = {}
+        for i in range(1, amount + 1):
+            self.dict['x'+str(i)] = True
+
+    def display_state(self):
+        for i in self.dict:
+            print (i,self.dict[i])
+
+    def generate_combintions(self, faults_list):
+        self.set_true()
+        for i in faults_list:
+            self.dict[i] = False
+
+    def set_true(self):
+        for i in self.dict:
+            self.dict[i] = True
+
+
---
+++ main.py
@@ -0,0 +1,33 @@
+import itertools
+from Processors import Processors
+
+__author__ = 'Milena'
+
+from sympy.parsing.sympy_parser import parse_expr
+# from sympy.abc import a, b
+
+def main():
+    faults = {1: '1 fault', 2: '2 faults', 3: '3 faults', 4: '4 faults', 5: '5 faults'}
+
+    processors_amount = 5
+    processors = Processors(processors_amount)
+
+    for i in faults:
+        print (faults[i])
+        faults_combination = 0
+        for j in itertools.combinations(processors.dict.keys(), i):
+            processors.generate_combintions(j)
+            amount_faults = 0
+            with open("formulas.txt", "r") as f:
+                for line in f:
+                    if not parse_expr(line).subs(processors.dict):
+                        amount_faults += 1
+            print 'For faults in processors '+str(j)+' there is '+str(amount_faults)+' faults in formulas'
+            if not amount_faults ==0:
+                faults_combination +=1
+        print str(faults_combination)+" combination failed from "+ str(len(list(itertools.combinations(processors.dict.keys(), i))))+' combinations'
+
+
+
+if __name__ == "__main__":
+    main()
